4b58ee848e5a0d22068e2d8e1518156035c5827d
Hu Wang <huw@codeaurora.org>
2021-10-14 17:38:33 +0800

    Wifi: Do not do Self Recovery when detect iface down on non-primary STA
    When secondary STA interface is deleted by HDM, it is possible that
    interface down happens during secondray STA cleanup, this trigger
    Wifi Self Recovery which makes exising primary Wi-Fi state loss.
    
    Change-Id: I6b974bd2ff8dce46a3f344cf54fc0bcc89c394c5
    CRs-Fixed: 3048815

--- service/java/com/android/server/wifi/ConcreteClientModeManager.java
+++ service/java/com/android/server/wifi/ConcreteClientModeManager.java
@@ -906,9 +906,14 @@ public class ConcreteClientModeManager implements ClientModeManager {
                         break;
                     }
                     case CMD_INTERFACE_DOWN:
-                        Log.e(getTag(), "Detected an interface down, reporting failure to "
-                                + "SelfRecovery");
-                        mSelfRecovery.trigger(SelfRecovery.REASON_STA_IFACE_DOWN);
+                        if (getRole() == ROLE_CLIENT_PRIMARY) {
+                            Log.e(getTag(), "Detected an interface down, reporting failure to "
+                                 + "SelfRecovery");
+                            mSelfRecovery.trigger(SelfRecovery.REASON_STA_IFACE_DOWN);
+                        } else {
+                            Log.i(getTag(), "Detected an interface down on non-primary STA, not "
+                                + "do SelfRecovery");
+                        }
                         // once interface down, nothing else to do...  stop the state machine
                         captureObituaryAndQuitNow();
                         break;
